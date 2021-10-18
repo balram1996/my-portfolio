735426a3a8150d50d82dff6aa1d9cf378fdacc8d
Balram <kurmibalram936@gmail.com>
2021-10-18 18:19:05 +0530

    changes 1

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!
                 <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
+                  
                 </span>
               </h1>
 
